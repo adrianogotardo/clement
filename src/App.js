483b2b7685e91b66933cdb743c908fd6b41e2b3d
Adriano Gotardo <adrianogotardo.12@gmail.com>
2024-09-30 22:46:15 -0300

    Initial commit (empty template)

---
+++ src/App.js
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <div className="App">
+      <header>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
